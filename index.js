01cf0915f9a55da75976dc4dd734f06a1f4049f2
Rogelio Alberto <i@rog.mx>
2016-01-19 13:40:00 +0000

    Init the project

---
+++ index.js
@@ -0,0 +1,9 @@
+import Twit from 'twit'
+import config from './config'
+
+const T = new Twit({
+  consumer_key: '...'
+  , consumer_secret:      '...'
+  , access_token:         '...'
+  , access_token_secret:  '...'
+})
