8333cd534c29ec3b0d02ef2052cf8074c1d62553
Rogelio Alberto <i@rog.mx>
2016-01-23 16:18:22 -0600

    Implement Async/Await - Use a promise to get the tweets and implement Async/Await to dipllay the results - Add and Configure ESLinter

--- app/app.js
+++ app/app.js
@@ -1,14 +1,25 @@
 import Twit from 'twit'
 import debug from 'debug'
 import config from './config'
+const deb = debug('app:FunkToFunky')
 
-const d = debug('app:funktofunky')
 const T = new Twit(config.twitter)
 
-T.get(
-  'search/tweets',
-  { q:'coldplay since:2011-11-11', count: 2 },
-  function(err, data, response) {
-    console.log(data)
-  }
-)
+const getTweets = () => {
+  return new Promise((resolve) => {
+    T.get(
+      'search/tweets',
+      { q: 'coldplay since:2011-11-11', count: 2 },
+      (err, data) => {
+        resolve(data)
+      }
+    )
+  })
+}
+
+async function printTweets() {
+  const tweets = await getTweets()
+  deb(tweets)
+}
+
+printTweets()
