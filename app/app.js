340a0c735efc210954d0b3d24befe75de98dbc6c
Rogelio Alberto <i@rog.mx>
2016-01-23 14:29:25 -0600

    Add .babelrc file and move App files

--- app/app.js
+++ app/app.js
@@ -1,11 +1,13 @@
 import Twit from 'twit'
+import debug from 'debug'
 import config from './config'
 
+const d = debug('app:funktofunky')
 const T = new Twit(config.twitter)
 
-T.post(
-  'statuses/update',
-  { status: 'hello world!' },
+T.get(
+  'search/tweets',
+  { q:'coldplay since:2011-11-11', count: 2 },
   function(err, data, response) {
     console.log(data)
   }
